c39695407fa7c59f5765a7ac4ff6124ec0065777
John Adebayo <johnayomide31@gmail.com>
2026-06-12 11:02:29 +0100

    "Add Democracy Day email with --list and --email options"

--- backend/app/Console/Commands/SendDemocracyDayEmails.php
+++ backend/app/Console/Commands/SendDemocracyDayEmails.php
@@ -12,7 +12,9 @@ class SendDemocracyDayEmails extends Command
 {
     protected $signature = 'democracy-day:send
         {--dry-run : Count recipients without sending}
-        {--user= : Send to a single user ID}';
+        {--user= : Send to a single user ID}
+        {--email= : Send to a specific email address}
+        {--list : List all users with IDs and emails}';
 
     protected $description = 'Send the Democracy Day greeting to all registered users.';
 
@@ -20,12 +22,23 @@ class SendDemocracyDayEmails extends Command
     {
         $dryRun = (bool) $this->option('dry-run');
         $userId = $this->option('user');
+        $targetEmail = $this->option('email');
+
+        if ($this->option('list')) {
+            $users = User::whereNotNull('email')->get(['id', 'email']);
+            $this->table(['ID', 'Email'], $users->toArray());
+            return self::SUCCESS;
+        }
 
         $query = User::query()
             ->whereNotNull('email');
 
         if ($userId) {
             $query->whereKey((int) $userId);
+        }
+
+        if ($targetEmail) {
+            $query->where('email', $targetEmail);
         }
 
         $total = $query->count();
